2852b82ce7b54ed4a03540db72f6fffaf589bef2
AayushP123 <108435719+AayushP123@users.noreply.github.com>
2026-07-18 23:00:04 -0700

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # Come from both sides, l, r
+        # If theres a mismatch, check if moving l + 1 would work, or r-1
+        # Else, return False
+        # Also remember, only ONE char can be deleted, so keep checking
+        # Until the string DOESN'T equal.
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                checkLeft = s[l + 1: r + 1]
+                checkRight = s[l : r]
+                return checkLeft == checkLeft[::-1] or checkRight == checkRight[::-1]
+            l = l + 1
+            r = r - 1
+        return True
